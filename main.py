dd1f5f4440bec631ca073ab9ae1955a532803be6
Hari Krishna Vemula <hari@harivemula.com>
2022-09-24 14:51:44 +0800

    added os and used path

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #import pandas as pd
 from fastapi import FastAPI
 from joblib import load
+import os
 
 app = FastAPI()
 
@@ -9,6 +10,7 @@ app = FastAPI()
 def predict_model (sl: float, sw: float, pl: float, pw: float):
     #input = np.array([iobj.sl, iobj.sw, iobj.pl, iobj.pw])
     #df = pd.DataFrame(data=input, columns=feature_names)
-    model = load('models/iris-sv-model')
+    model = load(os.path.join('./models', 'iris-sv-model'))
+    print(model)
     pred = model([[sl, sw, pl, pw]])
     return {'species': int(pred[0])}
