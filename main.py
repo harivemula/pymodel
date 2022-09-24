13a22fa76a3f43e32c2a25059863717f10de04cf
Hari Krishna Vemula <hari@harivemula.com>
2022-09-24 15:15:12 +0800

    using pandas to pass input to predict method instead of [[1,2,3,4]] format

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-#import numpy as np
-#import pandas as pd
+import numpy as np
+import pandas as pd
 from fastapi import FastAPI
 from joblib import load
 
 app = FastAPI()
-#feature_names = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+feature_names = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
 target_names = ['setosa', 'versicolor', 'virginica']
 @app.get("/predict")
 def predict_model (sl: float, sw: float, pl: float, pw: float):
-    #input = np.array([iobj.sl, iobj.sw, iobj.pl, iobj.pw])
-    #df = pd.DataFrame(data=input, columns=feature_names)
+    input = np.array([sl, sw, pl, pw])
+    df = pd.DataFrame(data=input, columns=feature_names)
     model = load('./models/iris-sv-model')
     print(model)
     pred = model.predict([[sl, sw, pl, pw]])
