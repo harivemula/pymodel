b724d414ac472fca1bd0dd8c983ae72deee2632d
Hari Krishna Vemula <hari@harivemula.com>
2022-09-24 14:38:06 +0800

    removed pydantics class

--- main.py
+++ main.py
@@ -2,26 +2,13 @@
 #import pandas as pd
 import fastapi as FastAPI
 from joblib import load
-from pydantic import BaseModel
-
-class Iris(BaseModel):
-    sl: float
-    sw: float
-    pl: float
-    pw: float
-
-feature_names = ['sepal length (cm)',
- 'sepal width (cm)',
- 'petal length (cm)',
- 'petal width (cm)']
-
 
 app = FastAPI()
 
 @app.get("/predict")
-def predict_model (iobj: Iris):
+def predict_model (sl: float, sw: float, pl: float, pw: float):
     #input = np.array([iobj.sl, iobj.sw, iobj.pl, iobj.pw])
     #df = pd.DataFrame(data=input, columns=feature_names)
     model = load('models/iris-sv-model')
-    pred = model([[iobj.sl, iobj.sw, iobj.pl, iobj.pw]])
+    pred = model([[sl, sw, pl, pw]])
     return {'species': int(pred[0])}
