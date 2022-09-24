252e6ee397242524d252f193c56b056ee9c35d42
Hari Krishna Vemula <hari@harivemula.com>
2022-09-24 13:53:54 +0800

    initial iris sample

---
+++ main.py
@@ -0,0 +1,27 @@
+#import numpy as np
+#import pandas as pd
+import fastapi as FastAPI
+from joblib import load
+from pydantic import BaseModel
+
+class Iris(BaseModel):
+    sl: float
+    sw: float
+    pl: float
+    pw: float
+
+feature_names = ['sepal length (cm)',
+ 'sepal width (cm)',
+ 'petal length (cm)',
+ 'petal width (cm)']
+
+
+app = FastAPI()
+
+@app.get("/predict")
+def predict_model (iobj: Iris):
+    #input = np.array([iobj.sl, iobj.sw, iobj.pl, iobj.pw])
+    #df = pd.DataFrame(data=input, columns=feature_names)
+    model = load('models/iris-sv-model')
+    pred = model([[iobj.sl, iobj.sw, iobj.pl, iobj.pw]])
+    return {'species': pred}
